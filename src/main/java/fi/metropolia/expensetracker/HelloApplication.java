f48e71ac5d0df46cc2ff427fcacb1424b4b33b3a
adnanavni <adnan.avni@hotmail.com>
2023-02-09 00:58:00 +0200

    Revert "pr stuff"
    This reverts commit 3a69592faa036b28ebb0a91755354883f8268912.

--- src/main/java/fi/metropolia/expensetracker/HelloApplication.java
+++ src/main/java/fi/metropolia/expensetracker/HelloApplication.java
@@ -15,7 +15,6 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        System.out.println("Hello World!");
     }
 
     public static void main(String[] args) {
