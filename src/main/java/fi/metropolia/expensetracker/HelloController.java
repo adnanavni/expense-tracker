da86d55e2444ac5570e2a25478cdff44340442ed
Perttu Harvala <harvala.perttu@gmail.com>
2023-02-09 14:27:02 +0200

    pertun branchi

--- src/main/java/fi/metropolia/expensetracker/HelloController.java
+++ src/main/java/fi/metropolia/expensetracker/HelloController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 public class HelloController {
     @FXML
     private Label welcomeText;
-
+    //pertun kommentti
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
