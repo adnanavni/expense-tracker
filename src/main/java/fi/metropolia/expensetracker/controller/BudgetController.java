95e55ae42b3213b3b2dcde557c509b749457a270
adnanavni <adnan.avni@hotmail.com>
2023-02-13 14:31:46 +0200

    Passing the same calculator object through all controllers

--- src/main/java/fi/metropolia/expensetracker/controller/BudgetController.java
+++ src/main/java/fi/metropolia/expensetracker/controller/BudgetController.java
@@ -1,17 +1,32 @@
 package fi.metropolia.expensetracker.controller;
 
 import fi.metropolia.expensetracker.MainApplication;
+import fi.metropolia.expensetracker.module.Calculator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.util.Currency;
 
 public class BudgetController {
 
     @FXML
     private AnchorPane content;
+
+    private Calculator calculator;
+    private Currency currency;
+
+    @FXML
+    private Label budget;
+
+    public void setCalculator(Calculator calculator) {
+        this.calculator = calculator;
+        currency = Currency.getInstance(calculator.getCurrentCurrency());
+        budget.setText(calculator.getBudget().toString() + " " + currency.getSymbol());
+    }
 
     public void backToMain(ActionEvent event) throws IOException {
         AnchorPane pane = FXMLLoader.load(MainApplication.class.getResource("main-view.fxml"));
