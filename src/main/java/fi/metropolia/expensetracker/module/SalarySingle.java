485af52cec6b7edca2ab50974eb5c92cbc13b6c8
IlonaJuv <ilonaju@metropolia.fi>
2023-02-16 23:53:59 +0200

    Alustava palkanlasku tuntimäärän perusteella. Pystyy lisätä historiassa olevat päivät yhteen ja näyttää niiden kokonaispalkkamäärän

---
+++ src/main/java/fi/metropolia/expensetracker/module/SalarySingle.java
@@ -0,0 +1,35 @@
+package fi.metropolia.expensetracker.module;
+
+import java.util.ArrayList;
+
+public class SalarySingle {
+
+
+    private double daySalary;
+    private ArrayList<Salary> salaries = new ArrayList<Salary>();
+
+    private static SalarySingle INSTANCE = null;
+    public static SalarySingle getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new SalarySingle();
+        return INSTANCE;
+    }
+
+    private SalarySingle(){}
+
+    public ArrayList<Salary> getDaySalaries(){
+
+        return salaries;
+    }
+    public double getDaySalary(){//return salary.getDaySalary();
+        return this.daySalary; }
+
+    public void createNewSalary(Salary salary) {
+        salaries.add(salary);
+    }
+    public void CalculateDaySalary(double hours, double hourSalary) {
+        this.daySalary = (hours*hourSalary);
+
+    }
+
+}
