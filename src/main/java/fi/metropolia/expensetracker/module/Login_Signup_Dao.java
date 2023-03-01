6dd402b02a6d61195e70a9f8a616089894a82f85
adnanavni <adnan.avni@hotmail.com>
2023-03-01 11:01:41 +0200

    registration and login visuals and controller fixes also added the mariadbdriver to pom file

--- src/main/java/fi/metropolia/expensetracker/module/Login_Signup_Dao.java
+++ src/main/java/fi/metropolia/expensetracker/module/Login_Signup_Dao.java
@@ -6,18 +6,16 @@ import java.sql.*;
 
 public class Login_Signup_Dao {
 
-    private static final String INSERT_QUERY = "INSERT INTO registration (full_name, email_id, password) VALUES (?, ?, ?)";
-    private static final String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
+    private static final String INSERT_QUERY = "INSERT INTO registration (full_name, password) VALUES (?, ?)";
+    private static final String SELECT_QUERY = "SELECT * FROM registration WHERE full_name = ? and password = ?";
 
-    private final Connection conn = MariaDBConnector.getInstance();
-    public void insertRecord(String fullName, String emailId, String password) throws SQLException {
+    public void insertRecord(String fullName, String password) throws SQLException {
 
-        try {
+        try (Connection connection = MariaDBConnector.getInstance()){
 
-             PreparedStatement preparedStatement = conn.prepareStatement(INSERT_QUERY);
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_QUERY);
              preparedStatement.setString(1, fullName);
-             preparedStatement.setString(2, emailId);
-             preparedStatement.setString(3, password);
+             preparedStatement.setString(2, password);
 
             System.out.println(preparedStatement);
             preparedStatement.executeUpdate();
@@ -26,12 +24,12 @@ public class Login_Signup_Dao {
         }
     }
 
-    public boolean validate(String emailId, String password) throws SQLException {
+    public boolean validate(String fullName, String password) throws SQLException {
 
-        try {
+        try (Connection connection   = MariaDBConnector.getInstance()) {
 
-             PreparedStatement preparedStatement = conn.prepareStatement(SELECT_QUERY);
-            preparedStatement.setString(1, emailId);
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_QUERY);
+            preparedStatement.setString(1, fullName);
             preparedStatement.setString(2, password);
 
             System.out.println(preparedStatement);
@@ -40,40 +38,14 @@ public class Login_Signup_Dao {
             if (resultSet.next()) {
                 return true;
             }
-            resultSet.close();
-            preparedStatement.close();
+
 
         }  catch (SQLException e) {
             printSQLException(e);
         }
         return false;
     }
-    public Integer loggedID(String emailId, String password){
-        try  {
 
-            String sql = "SELECT id FROM registration WHERE email_id = ? and password = ?";
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-
-            preparedStatement.setString(1, emailId);
-            preparedStatement.setString(2, password);
-
-            System.out.println(preparedStatement);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                return resultSet.getInt(1);
-
-            }
-            resultSet.close();
-            preparedStatement.close();
-
-
-        }  catch (SQLException e) {
-            printSQLException(e);
-        }
-
-        return null;
-    }
     public static void printSQLException(SQLException ex) {
         for (Throwable e: ex) {
             if (e instanceof SQLException) {
