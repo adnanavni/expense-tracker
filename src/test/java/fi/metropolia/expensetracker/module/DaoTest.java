bf95ec33593cbafd2bcad2cce9ef6811899f8ae7
IlonaJuv <ilonaju@metropolia.fi>
2023-03-05 20:29:05 +0200

    ei muutoksia

--- src/test/java/fi/metropolia/expensetracker/module/DaoTest.java
+++ src/test/java/fi/metropolia/expensetracker/module/DaoTest.java
@@ -1,5 +1,7 @@
 package fi.metropolia.expensetracker.module;
 
+import fi.metropolia.expensetracker.module.Dao.Dao;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -7,15 +9,17 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DaoTest {
-    Dao dao = new Dao();
-
+    static Dao dao = new Dao();
+    @AfterAll
+    static void removeTestUsers() throws SQLException {
+        dao.deleteUser("testExpense");
+    }
     @Test
     void userExists() throws SQLException {
         dao.insertRecord("testExpense", "testExpense");
         assertTrue(dao.userExists("testExpense"), "User not found" );
         dao.deleteUser("testExpense");
     }
-
     @Test
     void removeUser() throws SQLException {
         dao.insertRecord("testExpense", "testExpense");
--- src/test/java/fi/metropolia/expensetracker/module/SalarySingleTest.java
+++ src/test/java/fi/metropolia/expensetracker/module/SalarySingleTest.java
@@ -6,12 +6,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SalarySingleTest {
-
-    @Test
-    void geTotalSalaryOfMonth() {
-
-    }
-
     @Test
     @DisplayName("Day salary calculation")
     void calculateDaySalary() {
